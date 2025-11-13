0f112af9b2f812ba8c1436a35b61bab5572c44a4
Old-est <d.tupitsyn.side@gmail.com>
2025-11-13 13:03:00 +0400

    some pretty stuff
    Added python to emacs and toc to config.
    Added devdocs to nvim

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -64,6 +64,42 @@ return {
         ---@type render.md.UserConfig
         opts = {
             completions = { blink = { enabled = true } },
+            bullet = {
+                -- Turn on / off list bullet rendering
+                enabled = true,
+            },
+            checkbox = {
+                -- Turn on / off checkbox state rendering
+                enabled = true,
+                -- Determines how icons fill the available space:
+                --  inline:  underlying text is concealed resulting in a left aligned icon
+                --  overlay: result is left padded with spaces to hide any additional text
+                position = "inline",
+                unchecked = {
+                    -- Replaces '[ ]' of 'task_list_marker_unchecked'
+                    icon = "   󰄱 ",
+                    -- Highlight for the unchecked icon
+                    highlight = "RenderMarkdownUnchecked",
+                    -- Highlight for item associated with unchecked checkbox
+                    scope_highlight = nil,
+                },
+                checked = {
+                    -- Replaces '[x]' of 'task_list_marker_checked'
+                    icon = "   󰱒 ",
+                    -- Highlight for the checked icon
+                    highlight = "RenderMarkdownChecked",
+                    -- Highlight for item associated with checked checkbox
+                    scope_highlight = nil,
+                },
+            },
+            html = {
+                -- Turn on / off all HTML rendering
+                enabled = true,
+                comment = {
+                    -- Turn on / off HTML comment concealing
+                    conceal = false,
+                },
+            },
             latex = {
                 enabled = true,
                 converter = { 'utftex' },
@@ -84,65 +120,84 @@ return {
         opts = { -- set to setup table
         },
     },
-      {
-     "maskudo/devdocs.nvim",
-    lazy = false,
-    dependencies = {
-      "folke/snacks.nvim",
+    {
+        "maskudo/devdocs.nvim",
+        lazy = false,
+        dependencies = {
+            "folke/snacks.nvim",
+        },
+        cmd = { "DevDocs" },
+        keys = {
+            {
+                "<leader>ho",
+                mode = "n",
+                "<cmd>DevDocs get<cr>",
+                desc = "Get Devdocs",
+            },
+            {
+                "<leader>hi",
+                mode = "n",
+                "<cmd>DevDocs install<cr>",
+                desc = "Install Devdocs",
+            },
+            {
+                "<leader>hv",
+                mode = "n",
+                function()
+                    local devdocs = require("devdocs")
+                    local installedDocs = devdocs.GetInstalledDocs()
+                    vim.ui.select(installedDocs, {}, function(selected)
+                        if not selected then
+                            return
+                        end
+                        local docDir = devdocs.GetDocDir(selected)
+                        -- prettify the filename as you wish
+                        Snacks.picker.files({ cwd = docDir })
+                    end)
+                end,
+                desc = "Get Devdocs",
+            },
+            {
+                "<leader>hd",
+                mode = "n",
+                "<cmd>DevDocs delete<cr>",
+                desc = "Delete Devdoc",
+            }
+        },
+        opts = {
+            ensure_installed = {
+                "go",
+                "html",
+                "cpp",
+                -- "dom",
+                "http",
+                -- "css",
+                -- "javascript",
+                -- "rust",
+                -- some docs such as lua require version number along with the language name
+                -- check `DevDocs install` to view the actual names of the docs
+                "lua~5.1",
+                -- "openjdk~21"
+            },
+        },
     },
-    cmd = { "DevDocs" },
-    keys = {
-      {
-        "<leader>ho",
-        mode = "n",
-        "<cmd>DevDocs get<cr>",
-        desc = "Get Devdocs",
-      },
-      {
-        "<leader>hi",
-        mode = "n",
-        "<cmd>DevDocs install<cr>",
-        desc = "Install Devdocs",
-      },
-      {
-        "<leader>hv",
-        mode = "n",
-        function()
-          local devdocs = require("devdocs")
-          local installedDocs = devdocs.GetInstalledDocs()
-          vim.ui.select(installedDocs, {}, function(selected)
-            if not selected then
-              return
-            end
-            local docDir = devdocs.GetDocDir(selected)
-            -- prettify the filename as you wish
-            Snacks.picker.files({ cwd = docDir })
-          end)
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = '~/orgfiles/**/*',
+                org_default_notes_file = '~/orgfiles/refile.org',
+            })
+
+            -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+            -- add ~org~ to ignore_install
+            -- require('nvim-treesitter.configs').setup({
+            --   ensure_installed = 'all',
+            --   ignore_install = { 'org' },
+            -- })
         end,
-        desc = "Get Devdocs",
-      },
-      {
-        "<leader>hd",
-        mode = "n",
-        "<cmd>DevDocs delete<cr>",
-        desc = "Delete Devdoc",
-      }
-    },
-    opts = {
-      ensure_installed = {
-        "go",
-        "html",
-        "cpp",
-        -- "dom",
-        "http",
-        -- "css",
-        -- "javascript",
-        -- "rust",
-        -- some docs such as lua require version number along with the language name
-        -- check `DevDocs install` to view the actual names of the docs
-        "lua~5.1",
-        -- "openjdk~21"
-      },
-    },
-  },
+    }
 }
